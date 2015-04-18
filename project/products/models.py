364808191b4219f2df49118d74283a798c90d8f3
Elisabet Gonzalez <eligonzalez05@gmail.com>
2015-04-18 23:26:03 +0200

    He modificado el modelo Wine

--- project/products/models.py
+++ project/products/models.py
@@ -42,14 +42,15 @@ class Product(models.Model):
 
 class Wine(Product):
     size = models.CharField(max_length=50, blank=True)
-    alcohol = models.DecimalField(max_digits=4,decimal_places=2)
-    temperature = models.DecimalField(max_digits=4,decimal_places=2)
+    alcohol = models.DecimalField(max_digits=4,decimal_places=0)
+    temperature = models.CharField(max_length=1000, blank=True)
     elaboration = models.CharField(max_length=1000, blank=True)
     cellar = models.ForeignKey('Cellar', blank=True)
     zone = models.ForeignKey('Zone', blank=True)
     country = models.ForeignKey('Country', blank=True)
     varietal = models.ForeignKey('Varietal', blank=True)
     style = models.CharField(max_length=40, blank=True)
+    marriage = models.CharField(max_length=1000, blank=True)
     TYPE = (
         ('B', 'blanco'),
         ('T', 'tinto'),
