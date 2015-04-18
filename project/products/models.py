8ee6567b05f4f954c7ef73889908708c0faf62ac
Elisabet Gonzalez <eligonzalez05@gmail.com>
2015-04-19 00:37:39 +0200

    He modificado los modelos y el admin.py para que se muestren los objetos en forma de lista.

--- project/products/models.py
+++ project/products/models.py
@@ -3,53 +3,51 @@ from django.db import models
 
 class Cellar(models.Model):
     name = models.CharField(max_length=50)
-
     def __str__(self):
         return self.name
-
 
 class Zone(models.Model):
     name = models.CharField(max_length=50)
-
     def __str__(self):
         return self.name
-
 
 class Country(models.Model):
     name = models.CharField(max_length=50)
-
     def __str__(self):
         return self.name
-
 
 class Varietal(models.Model):
     name = models.CharField(max_length=50)
-
     def __str__(self):
         return self.name
 
+class SubTipeSpirit(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
-
+class Style(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to="static/images/shop/products")
-
+    zone = models.ForeignKey('Zone', blank=True, default=None)
+    country = models.ForeignKey('Country', blank=True, default=None)
+    elaboration = models.CharField(max_length=1000, blank=True, default=None)
+    alcohol = models.CharField(max_length=1000, blank=True, default=None)
     def __str__(self):
         return self.name
 
-
 class Wine(Product):
     size = models.CharField(max_length=50, blank=True)
-    alcohol = models.DecimalField(max_digits=4,decimal_places=0)
     temperature = models.CharField(max_length=1000, blank=True)
-    elaboration = models.CharField(max_length=1000, blank=True)
     cellar = models.ForeignKey('Cellar', blank=True)
-    zone = models.ForeignKey('Zone', blank=True)
-    country = models.ForeignKey('Country', blank=True)
     varietal = models.ForeignKey('Varietal', blank=True)
-    style = models.CharField(max_length=40, blank=True)
+    style = models.CharField('Style', max_length=50, blank=True)
     marriage = models.CharField(max_length=1000, blank=True)
     TYPE = (
         ('B', 'blanco'),
@@ -59,3 +57,10 @@ class Wine(Product):
     )
     type = models.CharField(max_length=1, choices=TYPE, default=False, blank=True)
 
+class Spirit(Product):
+    TIPE = (
+        ('D', 'Destilado'),
+        ('L', 'Licor'),
+    )
+    tipe = models.CharField(max_length=1, choices=TIPE, default='D',blank=True)
+    subTipe = models.ForeignKey(SubTipeSpirit,blank=True)
