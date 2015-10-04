7eb46dfda83e1bc9907a86c49338bd65dacaeffd
eligonzalez <eli.gonzalez05@gmail.com>
2015-10-04 20:07:24 +0200

    Cambio puntuacion producto, hecho!

--- project/static/project/general.js
+++ project/static/project/general.js
@@ -16,6 +16,38 @@
                 $('#changePassword').slideDown().show( "slow" );
             }
         });
+
+        if($('#raty')) {
+            var hints =  ['Malo', 'Pobre', 'Regular', 'Bueno', 'Excelente']
+            $('#raty').raty({score: 3,hints: hints, half: true,path: '/static/project/lib/images'});
+             var avarage_score = 0;
+             var comments = 0;
+             $("div[name='past_score']").each(function(){
+
+                var params = {};
+                params.score = $(this).text();
+                params.path = '/static/project/lib/images';
+                params.readOnly = true;
+                $(this).text('');
+                $(this).raty(params);
+                avarage_score += parseFloat(params.score);
+                ++comments;
+             });
+
+
+
+             var params = {};
+             params.score = avarage_score/comments;
+             params.path = '/static/project/lib/images';
+             params.readOnly = true;
+             $("#avarage_score").raty(params);
+
+        };
+
+        $('#comment_submit').on('click', function (){
+            $("input[name='score']").val()
+        });
+
     });
 
 
